b0b44db094b6f506e14cd2ebd889be2706d17647
kououdev <kououdev@gmail.com>
2025-10-18 19:53:25 +0700

    fix bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
     <div class="container py-5">
         @yield('content')
     </div>
-    @yield('scripts')
-
     <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+    @yield('scripts')
 </body>
 
 </html>
