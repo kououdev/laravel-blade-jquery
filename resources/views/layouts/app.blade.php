f6a41e392655c19e0f5e428616c823d14c54706a
kououdev <kououdev@gmail.com>
2025-10-18 19:47:06 +0700

    add external files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Customer CRUD | Laravel + jQuery + Blade</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 
 <body class="bg-light">
@@ -13,6 +12,9 @@
         @yield('content')
     </div>
     @yield('scripts')
+
+    <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 
 </html>
