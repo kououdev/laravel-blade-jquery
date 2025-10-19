05b4339afbcb6403fe2e3ddcf8689f9ab835b179
kououdev <kououdev@gmail.com>
2025-10-19 22:09:21 +0700

    update dashboard 65%

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row text-muted">
+            <div class="col-6 text-start">
+                <p class="mb-0">
+                    <a class="text-muted" href="https://github.com/kououdev" target="_blank">
+                        <strong>Kououdev</strong>
+                    </a>
+                    -
+                    <a class="text-muted" href="https://github.com/kououdev/laravel-blade-jquery" target="_blank">
+                        <strong>Laravel Blade JQuery</strong>
+                    </a> &copy;
+                </p>
+            </div>
+            <div class="col-6 text-end">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="#" target="_blank">Support</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="#" target="_blank">Help Center</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="#" target="_blank">Privacy</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="#" target="_blank">Terms</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
